9585ecac53a4b454e29e385fc7211f683697a692
Logan Paton <pumpkinpotato5@gmail.com>
2016-04-25 14:50:06 -0500

    Update start.py
    Fixed a little mess up. Forgot the ) for the inputs!

--- start.py
+++ start.py
@@ -1,5 +1,5 @@
-star=input("Hello, what is your name? "
-que=input("And how are you feeling? "
+star=input("Hello, what is your name? ")
+que=input("And how are you feeling? ")
 print("You are "+star)
 if (que="Happy"):
   print("And you are one happy fellow!")

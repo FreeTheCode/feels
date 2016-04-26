badd495240114d002d31d8b9c06f1efbc39f43ef
Logan Paton <pumpkinpotato5@gmail.com>
2016-04-26 12:39:06 -0500

    Update start.py

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 star=input("Hello, what is your name? ")
 sta=input("And how are you feeling? ")
 print("You are "+star)
-if (sta="Happy"):
+if (sta=="Happy"):
   print("And you are one happy fellow!")
 else:
   print("And you are feeling "+sta)
